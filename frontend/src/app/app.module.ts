eb0481b0406724367a4be28325936bc991ceade9
Isabel Elizondo <isabel.elizondo.meza@est.una.ac.cr>
2019-11-04 22:23:28 -0600

    Componentes
    Se agregaron los componentes necesarios para trabajar en el frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { AdminComponent } from './componentes/admin/admin.component';
+import { ReservacionComponent } from './componentes/reservacion/reservacion.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { AgendaComponent } from './componentes/agenda/agenda.component';
+import { ReservaComponent } from './componentes/reserva/reserva.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    ReservacionComponent,
+    ConfiguracionComponent,
+    AgendaComponent,
+    ReservaComponent
   ],
   imports: [
     BrowserModule,
