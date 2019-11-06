5743e8f3619130748bd569e1ab065b6aab0f6845
Isabel Elizondo <isabel.elizondo.meza@est.una.ac.cr>
2019-11-06 08:30:32 -0600

    Elimino
    Proyecto Angular

--- backend/application/controllers/api/Reserva.php
+++ backend/application/controllers/api/Reserva.php
@@ -16,16 +16,17 @@ class Reserva extends REST_Controller {
         {
             $id = $this->uri->segment(4);
 
-            if(!empty($id)){
-                $data = $this->db->get_where($this->reserbaTb, ['id' => $id])->row_array();
-            }else{
-                $data = $this->db->get($this->reserbaTb)->result();
-            }
+            echo $id;
+            // if(!empty($id)){
+            //     $data = $this->db->get_where($this->reserbaTb, ['r_id' => $id])->row_array();
+            // }else{
+            //     $data = $this->db->get($this->reserbaTb)->result();
+            // }
 
-            if(!$data)
-                $data = 'No hay reservaciones con este ID.';
+            // if(!$data)
+            //     $data = 'No hay reservaciones con este ID.';
 
-            $this->response($data, REST_Controller::HTTP_OK);
+            // $this->response($data, REST_Controller::HTTP_OK);
         }
 
         public function insertReserva_post()
